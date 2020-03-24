82b17ec533c6d41fa4d6cf5b18109870fefee8f0
Dima <dima.avanesov@yandex.ru>
2020-03-24 16:47:07 +0700

    Fifth task done (v1)

--- node/2/custom-elements/my-leaf.js
+++ node/2/custom-elements/my-leaf.js
@@ -1,11 +1,28 @@
 import { LitElement, html } from 'lit-element';
 
 class MyLeaf extends LitElement {
+  static get properties() {
+    return { leaf: Object };
+  }
+
   render() {
+    this.leaf = JSON.parse(this.leaf);
+
     return html`
-      <ul>
-        ${this.structure.items.map(i => html`<li id="${i.id}">${i.id}</li>`)}
-      </ul>
+      <li id="${this.leaf.id}">
+        ${this.leaf.id}
+        ${'items' in this.leaf ? html`
+          <ul>${this.renderChildren(this.leaf.items)}</ul>
+        ` : html``}
+      </li>
+    `;
+  }
+
+  renderChildren(children) {
+    return html`
+      ${children.map(child => html`
+        <my-leaf leaf=${JSON.stringify(child)}></my-leaf>
+      `)}
     `;
   }
 }
--- node/2/custom-elements/my-tree.js
+++ node/2/custom-elements/my-tree.js
@@ -7,24 +7,10 @@ class MyTree extends LitElement {
   }
 
   render() {
-    this.structure = new Array(JSON.parse(this.structure));
-
     return html`
       <ul>
-        ${this.renderLevel(this.structure)}
+        <my-leaf leaf=${this.structure}></my-leaf>
       </ul>
-    `;
-  }
-
-  renderLevel(level) {
-    return html`
-      ${level.map(leaf => html`
-        <li id="${leaf.id}">
-          ${leaf.id}
-          ${'items' in leaf ? html`
-            <ul>${this.renderLevel(leaf.items)}</ul>
-          ` : html``}</li>
-      `)}
     `;
   }
 }
