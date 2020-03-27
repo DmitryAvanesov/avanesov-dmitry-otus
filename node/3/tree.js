445da19d2c0c227c5cf69dc41eb1aa1c1ab5b79b
Dima <dima.avanesov@yandex.ru>
2020-03-27 16:17:49 +0700

    Works but w/out Promise

--- node/3/tree.js
+++ node/3/tree.js
@@ -1,18 +1,25 @@
 import * as fs from 'fs';
 
-const root = process.argv[2];
-
-const handlePath = path => {
+const pathToObject = (path, tree) => {
   fs.readdir(path, (err, files) => {
     files.forEach(file => {
-      console.log(file);
       const innerPath = `${path}/${file}`;
 
       if (fs.lstatSync(innerPath).isDirectory()) {
-        handlePath(`${path}/${file}`);
+        tree.dirs.push(innerPath);
+        tree = pathToObject(innerPath, tree);
+      }
+
+      if (fs.lstatSync(innerPath).isFile()) {
+        tree.files.push(innerPath);
       }
     });
   });
+
+  return tree;
 }
 
-handlePath(root);
+const root = process.argv[2];
+const tree = pathToObject(root, { files: [], dirs: [] });
+
+setTimeout(() => console.log(tree), 1000);
