80f7550c75bb08fd25243be6d77e149c343df934
Dima <dima.avanesov@yandex.ru>
2020-04-01 13:59:38 +0700

    Number of files that can be handled by RAM

--- node/4/script.js
+++ node/4/script.js
@@ -1,6 +1,7 @@
-import * as fs from 'fs';
+const fs = require('fs');
+const LineByLineReader = require('line-by-line');
 
-const readStream = fs.createReadStream('./data/numbers.txt');
+const separatedFilesDirectory = './data/separated-files'
 
 const writeFile = (chunks, curFile) => {
   let chunksString = '';
@@ -10,34 +11,37 @@ const writeFile = (chunks, curFile) => {
   });
 
   const array = chunksString.split('\n').map(x => parseInt(x));
+  array.pop();
   array.sort((a, b) => a - b);
 
   const writeStream = fs.createWriteStream(`./data/separated-files/numbers${curFile}.txt`);
   writeStream.write(array.join('\n'));
 };
 
-const start = async () => {
+const createSeparatedSortedFiles = async () => {
   let curFile = 0;
   let chunks = [];
-  
-  let directory = './data/separated-files/'
 
   // clearing separate-files directory
-  fs.readdir(directory, (err, files) => {
+  fs.readdir(separatedFilesDirectory, (err, files) => {
     if (err) throw err;
-  
+
     for (const file of files) {
-      fs.unlink(`${directory}/${file}`, err => {
+      fs.unlink(`${separatedFilesDirectory}/${file}`, err => {
         if (err) throw err;
       });
     }
   });
 
+  const readStream = fs.createReadStream('./data/numbers.txt');
+  const minDataSize = 5;
+
   for await (const chunk of readStream) {
     chunks.push(chunk);
 
     // writing data into file if the number at the end of the current buffer isn't cut
-    if (chunk.toString().slice(-1) === '\n') {
+    // and the data is big enough
+    if (chunk.toString().slice(-1) === '\n' && chunks.length >= minDataSize) {
       writeFile(chunks, curFile++);
       chunks = [];
     }
@@ -46,6 +50,46 @@ const start = async () => {
   if (chunks.length > 0) {
     writeFile(chunks, curFile);
   }
+
+  fs.readdir(separatedFilesDirectory, (err, files) => {
+    sortAllNumbers(files.length);
+  });
 };
 
-start();
+const sortAllNumbers = async numberOfSeparatedFiles => {
+  const lineReadersArray = [];
+  let nums = [];
+
+  for (let i = 0; i < numberOfSeparatedFiles; i++) {
+    const readStream = fs.createReadStream(`${separatedFilesDirectory}/numbers${i}.txt`);
+    const lineReader = new LineByLineReader(readStream);
+
+    lineReader.on('line', line => {
+      lineReader.pause();
+      nums.push(line);
+    });
+
+    lineReader.on('end', () => {
+      // All lines are read, file is closed now.
+    });
+
+    lineReadersArray.push(lineReader);
+  }
+
+  setTimeout(() => console.log(nums), 2000);
+
+  // readStreamArray.forEach(async curStream => {
+  //   const readlineInterface = readline.createInterface({
+  //     input: curStream
+  //   });
+
+  //   for await (const line of readlineInterface) {
+  //     nums.push(parseInt(line));
+  //     curStream.pause();
+  //   }
+
+  //   console.log(nums);
+  // });
+};
+
+createSeparatedSortedFiles();
