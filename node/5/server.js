cdb05d18347a5bd6dabe3adec5811071c46fa571
Dima <dima.avanesov@yandex.ru>
2020-04-03 18:56:04 +0700

    Basic pages' structure

--- node/5/server.js
+++ node/5/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import Parser from 'rss-parser';
+import path from 'path';
 
 const app = express();
 const port = 3000;
+const parser = new Parser();
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.sendFile('./pages/index.html', { root: path.resolve() });
+});
+
+app.get('/create', (req, res) => {
+  res.send('Creation');
 });
 
 app.listen(port);
+
+const parseRSS = async () => {
+  const feed = await parser.parseURL('https://www.reddit.com/.rss');
+  console.log(feed.title);
+
+  feed.items.forEach(item => {
+    console.log(item.title + ':' + item.link)
+  });
+};
