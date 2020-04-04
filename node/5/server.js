3acd33c1b9c1e822a5fbb845436cdb79ecc70e56
Dima <dima.avanesov@yandex.ru>
2020-04-04 19:14:05 +0700

    MongoDB RSS parsing works

--- node/5/server.js
+++ node/5/server.js
@@ -1,23 +1,82 @@
 import express from 'express';
 import Parser from 'rss-parser';
-import path from 'path';
+import mongoose from 'mongoose';
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 const port = 3000;
 const parser = new Parser();
 
+const connectionString = 'mongodb+srv://admin:7CEWSY9f5y7xUQYR@cluster0-o95xp.gcp.mongodb.net/otus-node-5?retryWrites=true&w=majority';
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Url = mongoose.model('Url', {
+  address: String
+});
+
+const Document = mongoose.model('Document', {
+  title: String,
+  link: String,
+  pubDate: String,
+  creator: String,
+  content: String,
+  contentSnippet: String,
+  guid: String,
+  categories: [String],
+  isoDate: String
+});
+
+
 app.get('/', (req, res) => {
-  res.sendFile('./pages/index.html', { root: path.resolve() });
+  res.render('index');
 });
 
 app.get('/create', (req, res) => {
-  console.log('Got');
-  res.sendFile('./pages/create.html', { root: path.resolve() });
+  res.render('create', {
+    result: ''
+  });
 });
 
-app.post('/create', (req, res) => {
-  console.log('Posted');
-  res.redirect('/show-feed');
+app.post('/create', async (req, res) => {
+  try {
+    const feed = await parser.parseURL(req.body.url);
+
+    Url.find({ address: req.body.url }, async (err, docs) => {
+      if (err) throw err;
+
+      const addDocuments = () => {
+        feed.items.forEach(async item => {
+          const document = new Document(item);
+          await document.save();
+        });
+      };
+
+      if (docs.length == 0) {
+        const url = new Url({ address: req.body.url });
+        await url.save();
+
+        addDocuments();
+    
+        res.render('create', {
+          result: 'Success: documents and URL added'
+        });
+      }
+      else {
+        addDocuments();
+
+        res.render('create', {
+          result: 'Success: documents added, URL was already in the database'
+        });
+      }
+    });
+  }
+  catch (err) {
+    res.render('create', {
+      result: 'Error: URL is incorrect'
+    });
+  }
 });
 
 app.get('/show-feed', (req, res) => {
@@ -25,12 +84,3 @@ app.get('/show-feed', (req, res) => {
 });
 
 app.listen(port);
-
-const parseRSS = async () => {
-  const feed = await parser.parseURL('https://www.reddit.com/.rss');
-  console.log(feed.title);
-
-  feed.items.forEach(item => {
-    console.log(item.title + ':' + item.link)
-  });
-};
