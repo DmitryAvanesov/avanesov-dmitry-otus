06f72e07ccca6dc6fcd5ad139f605991e850f142
Dima <dima.avanesov@yandex.ru>
2020-06-11 18:00:16 +0700

    Game view

---
+++ vue/src/views/Game.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="game">
+   
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Game",
+  computed: mapState([
+    "duration",
+    "difficulty",
+    "types"
+  ]),
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
