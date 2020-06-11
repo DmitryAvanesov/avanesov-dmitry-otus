0921d0e2c399ea6ec2cf1200c8382b5d4f4c31a3
Dima <dima.avanesov@yandex.ru>
2020-06-11 18:41:39 +0700

    Cancel button, timer

--- vue/src/views/Game.vue
+++ vue/src/views/Game.vue
@@ -1,25 +1,36 @@
 <template>
   <div id="game">
-   
+    <router-link class="cancel-button" to="/settings">Cancel</router-link>
+    <timer />
   </div>
 </template>
 
 <script>
+import Timer from "../components/Timer";
 import { mapState } from "vuex";
 
 export default {
   name: "Game",
-  computed: mapState([
-    "duration",
-    "difficulty",
-    "types"
-  ]),
-  methods: {
-
+  computed: mapState(["duration", "difficulty", "types"]),
+  components: {
+    timer: Timer
   }
 };
 </script>
 
 <style scoped lang="scss">
+#game {
+  margin: 20px 15px;
+  font-size: 18px;
 
+  .cancel-button {
+    background-color: #0075ff;
+    border: none;
+    border-radius: 5%;
+    padding: 10px 15px;
+    font-size: 22px;
+    text-decoration: none;
+    color: white;
+  }
+}
 </style>
